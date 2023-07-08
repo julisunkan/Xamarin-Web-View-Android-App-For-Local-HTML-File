948105e81f751c913d233b65bceffb6399092189
Julius <Julius@INBOOK_X1_PRO>
2023-07-08 19:51:16 +0100

    Add project files.

---
+++ App3/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using AndroidX.AppCompat.App;
+using AndroidX.AppCompat.Widget;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using System;
+
+namespace App3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            WebView webView = FindViewById<WebView>(Resource.Id.ola);
+            webView.SetWebViewClient(new WebViewClient());
+            webView.LoadUrl("file:///android_asset/netman.html");
+            webView.Settings.JavaScriptEnabled = true;
+            webView.Settings.BuiltInZoomControls = true;
+            webView.Settings.SetSupportZoom(true);
+            webView.ScrollBarStyle = ScrollbarStyles.OutsideOverlay;
+            webView.ScrollbarFadingEnabled = false;
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View)sender;
+            Snackbar.Make(view, "Email: babajolami@gmail.com", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
